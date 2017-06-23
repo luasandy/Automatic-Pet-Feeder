33811c5437015f24fda37717500fab9a666f3651
Sandra <sandralvarez_2492@hotmail.com>
2017-06-23 14:58:24 -0500

    Add files via upload
    library that allows to send a push notification to an smartphone devise.

---
+++ Raspberry-Pi/push.py
@@ -0,0 +1,18 @@
+#Importamos algunas librerias necesarias
+import urllib2
+import json
+
+#Esta es la funcion que usa el script para enviar las notificaciones
+def sendNotification(token, channel, message):
+	data = {
+		"body" : message,
+		"message_type" : "text/plain"
+	}
+
+	req = urllib2.Request('http://api.pushetta.com/api/pushes/{0}/'.format(channel))
+	req.add_header('Content-Type', 'application/json')
+	req.add_header('Authorization', 'Token {0}'.format(token))
+
+	response = urllib2.urlopen(req, json.dumps(data))
+
+
